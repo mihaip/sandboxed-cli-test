db299392d0f480cb93cadf3a6316a5fd4ec94151
Mihai Parparita <mihai@tailscale.com>
2022-06-30 12:06:50 -0700

    Initial Commit

---
+++ SandboxedCLI/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  SandboxedCLI
+//
+//  Created by Mihai Parparita on 6/30/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
